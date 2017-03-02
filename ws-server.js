ef2d908c7c53156d5df0c4ff3e8950a555325b7a
Tom Jorquera <tom.jorquera@linagora.com>
2017-03-02 12:14:38 +0100

    conferences handling is now a separate module

--- ws-server.js
+++ ws-server.js
@@ -20,67 +20,13 @@ const recoSummaryAPIEndpoint = 'localhost';
 const recoSummaryAPIPort = 8090;
 
 const jobProcessing = require('./lib/jobProcessing.js');
+const conferencesHandler = require('./lib/conferencesHandler.js');
 const speechProcessing = require('./lib/speechengine/thirdparties/microsoft-cognitive/cognitive.js');
 
 speechProcessing.setup({
   key: # YOUR KEY HERE #,
   renewal: 8 * 60 * 1000
 });
-
-////////////////////////////////////////////////
-// Conferences Management
-////////////////////////////////////////////////
-
-class Conference {
-  constructor(confId) {
-    this.id = confId;
-    this.ws = new Set([]);
-    this.chunks = [];
-  }
-}
-
-
-let conferencesHandler = {
-
-  confs: {},
-
-  register: function(confId, ws) {
-    // ensure conf exists
-    if(!(confId in this.confs)) {
-      this.confs[confId] = new Conference(confId);
-    }
-    this.confs[confId].ws.add(ws);
-  },
-
-  unregister: function(confId, ws) {
-    if(!(confId in this.confs)) {
-      console.error('trying to unregister to non-existing conf ' + confId);
-      return;
-    }
-    this.confs[confId].ws.delete(ws);
-
-    // it was the last registered ws for the conf
-    if(this.confs[confId].ws.size == 0) {
-      delete this.confs[confId];
-    }
-  },
-
-  saveTranscriptChunk: function(confId, chunk) {
-    if(!(confId in this.confs)) {
-      console.error('trying to saveTranscriptionChunk to non-existing conf ' + confId);
-      return;
-    }
-    this.confs[confId].chunks.push(chunk);
-  },
-
-  pushEvent: function(confId, event) {
-    if(!(confId in this.confs)) {
-      console.error('trying to saveTranscriptionChunk to non-existing conf ' + confId);
-      return;
-    }
-    this.confs[confId].ws.forEach((ws) => { ws.send(event); });
-  }
-};
 
 ////////////////////////////////////////////////
 // Online reco
